6bb2ea5e06998bf21a5a10fd16255a40e0a7c861
Tom Anderson <tom.h.anderson@gmail.com>
2015-03-22 18:43:11 -0600

    cleaned module

--- config/module.config.php
+++ config/module.config.php
@@ -1,95 +1,17 @@
 <?php
+
 return array(
     'service_manager' => array(
+        'invokables' => array(
+            'ZF\OAuth2\Provider\UserId' => 
+                'ZF\OAuth2\Provider\UserId\AuthenticationService',
+        ),
         'abstract_factories' => array(
             'ZF\OAuth2\Factory\DoctrineMapperFactory',
         ),
         'factories' => array(
-            'ZF\OAuth2\Adapter\DoctrineAdapter' => 'ZF\OAuth2\Factory\DoctrineAdapterFactory',
-        ),
-    ),
-    'controllers' => array(
-        'factories' => array(
-            'ZF\OAuth2\Controller\Auth' => 'ZF\OAuth2\Factory\AuthControllerFactory',
-        ),
-    ),
-    'router' => array(
-        'routes' => array(
-            'oauth' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route'    => '/oauth',
-                    'defaults' => array(
-                        'controller' => 'ZF\OAuth2\Controller\Auth',
-                        'action'     => 'token',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'authorize' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Literal',
-                        'options' => array(
-                            'route' => '/authorize',
-                            'defaults' => array(
-                                'action' => 'authorize',
-                            ),
-                        ),
-                    ),
-                    'resource' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Literal',
-                        'options' => array(
-                            'route' => '/resource',
-                            'defaults' => array(
-                                'action' => 'resource',
-                            ),
-                        ),
-                    ),
-                    'code' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Literal',
-                        'options' => array(
-                            'route' => '/receivecode',
-                            'defaults' => array(
-                                'action' => 'receiveCode',
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
-    'view_manager' => array(
-        'template_map' => array(
-            'oauth/authorize'    => __DIR__ . '/../view/zf/auth/authorize.phtml',
-            'oauth/receive-code' => __DIR__ . '/../view/zf/auth/receive-code.phtml',
-        ),
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
-    'zf-oauth2' => array(
-        /*
-         * Config can include:
-         * - 'storage' => 'name of storage service' - typically ZF\OAuth2\Adapter\PdoAdapter
-         * - 'db' => [ // database configuration for the above PdoAdapter
-         *       'dsn'      => 'PDO DSN',
-         *       'username' => 'username',
-         *       'password' => 'password'
-         *   ]
-         * - 'storage_settings' => [ // configuration to pass to the storage adapter
-         *       // see https://github.com/bshaffer/oauth2-server-php/blob/develop/src/OAuth2/Storage/Pdo.php#L57-L66
-         *   ]
-         */
-    ),
-    'zf-content-negotiation' => array(
-        'ZF\OAuth2\Controller\Auth' => array(
-            'ZF\ContentNegotiation\JsonModel' => array(
-                'application/json',
-                'application/*+json',
-            ),
-            'Zend\View\Model\ViewModel' => array(
-                'text/html',
-                'application/xhtml+xml',
-            ),
+            'ZF\OAuth2\Adapter\DoctrineAdapter' => 
+                'ZF\OAuth2\Factory\DoctrineAdapterFactory',
         ),
     ),
 );

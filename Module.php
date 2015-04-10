ded9204443645175427706b4f26215af28132993
Jesus Vieira <jesusvieiradelima@gmail.com>
2015-04-10 15:56:17 -0300

    Conflict in the configuration #1

--- Module.php
+++ Module.php
@@ -3,7 +3,7 @@
 namespace ZF\OAuth2\Doctrine;
 
 use Zend\ModuleManager\ModuleManager;
-use ZF\OAuth2\EventListener\UserClientSubscriber;
+use ZF\OAuth2\Doctrine\EventListener\DynamicMappingSubscriber;
 use Doctrine\ORM\Mapping\Driver\XmlDriver;
 
 class Module
@@ -14,27 +14,28 @@ class Module
         $sm      = $app->getServiceManager();
         $config = $sm->get('Config');
 
+
         // Enable default entities
-        if (isset($config['zf-oauth2-doctrine']['storage_settings']['default_entities_enable'])
-            && $config['zf-oauth2-doctrine']['storage_settings']['default_entities_enable']) {
-            $chain = $sm->get($config['zf-oauth2-doctrine']['storage_settings']['driver']);
+        if (isset($config['zf-oauth2']['storage_settings']['enable_default_entities'])
+            && $config['zf-oauth2']['storage_settings']['enable_default_entities']) {
+            $chain = $sm->get($config['zf-oauth2']['storage_settings']['driver']);
             $chain->addDriver(new XmlDriver(__DIR__ . '/config/orm'), 'ZF\OAuth2\Entity');
         }
 
         // Enable default documents
-        if (isset($config['zf-oauth2-doctrine']['storage_settings']['default_documents_enable'])
-            && $config['zf-oauth2-doctrine']['storage_settings']['default_documents_enable']) {
-            $driver = $config['zf-oauth2-doctrine']['storage_settings']['default_documents_driver'];
-            $chain = $sm->get($config['zf-oauth2-doctrine']['storage_settings']['driver']);
+        if (isset($config['zf-oauth2']['storage_settings']['default_documents_enable'])
+            && $config['zf-oauth2']['storage_settings']['default_documents_enable']) {
+            $driver = $config['zf-oauth2']['storage_settings']['default_documents_driver'];
+            $chain = $sm->get($config['zf-oauth2']['storage_settings']['driver']);
             $chain->addDriver(new $driver(__DIR__ . '/config/odm'), 'ZF\OAuth2\Document');
         }
 
-        if (isset($config['zf-oauth2-doctrine']['storage_settings']['dynamic_mapping'])
-            && $config['zf-oauth2-doctrine']['storage_settings']['dynamic_mapping']) {
+        if (isset($config['zf-oauth2']['storage_settings']['dynamic_mapping'])
+            && $config['zf-oauth2']['storage_settings']['dynamic_mapping']) {
 
-            $userClientSubscriber = new UserClientSubscriber($config['zf-oauth2-doctrine']['storage_settings']['dynamic_mapping']);
+            $userClientSubscriber = new DynamicMappingSubscriber($config['zf-oauth2']['storage_settings']['dynamic_mapping']);
 
-            $eventManager = $sm->get($config['zf-oauth2-doctrine']['storage_settings']['event_manager']);
+            $eventManager = $sm->get($config['zf-oauth2']['storage_settings']['event_manager']);
 
             // $em is an instance of the Event Manager
             $eventManager->addEventSubscriber($userClientSubscriber);
